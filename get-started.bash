da73f214e521b06ebb318153cf9ecc881419d23b
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2017-10-20 16:03:17 +0300

    Try cloning into root at CSC

--- get-started.bash
+++ get-started.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 ## Script that downloads the code for doing the Deep Learning course exercises
 
-cd /home/jovyan/work
+cd /home/jovyan
 
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
-git clone https://github.com/HY-TKTL/deep-learning-course-2017.git
+git clone https://github.com/HY-TKTL/deep-learning-course-2017.git .
 
 #pip install requests_oauthlib
